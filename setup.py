4531b70ed5788e3f551aee2deb0f1effad2b9d61
Sahak <68821444+sahakyansahak@users.noreply.github.com>
2020-07-30 18:19:24 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ds18b20S", # Replace with your own username
+    name="ds18b20S",
     version="0.0.1",
     author="Sahak",
     author_email="sahak.sahakyan2017@gmail.com",
